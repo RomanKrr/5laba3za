de143161fd7b1bc196d3ff496ecf55d7e73a5e5d
RomanKrr <158074611+RomanKrr@users.noreply.github.com>
2024-05-09 11:57:55 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+document.getElementById('e').textContent = Math.E;
+document.getElementById('pi').textContent = Math.PI;
+document.getElementById('log10e').textContent = Math.LOG10E;
+document.getElementById('log2e').textContent = Math.LOG2E;
+document.getElementById('ln10').textContent = Math.LN10;
+document.getElementById('ln2').textContent = Math.LN2;
+document.getElementById('sqrt2').textContent = Math.SQRT2;
+document.getElementById('sqrt1_2').textContent = Math.SQRT1_2;
+
+// Функції
+document.getElementById('pow').textContent = Math.pow(4, 2);
+document.getElementById('sqrt').textContent = Math.sqrt(4);
+document.getElementById('sqrtNeg').textContent = Math.sqrt(-9);
+document.getElementById('abs').textContent = Math.abs(-9);
+document.getElementById('cos').textContent = Math.cos(0);
+
+// Генератор випадкових чисел
+document.getElementById('random').textContent = Math.random();
